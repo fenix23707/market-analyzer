ce68ec98a9c5ae32e5743579971ebe17c3fb7940
Vlad Kovzov <fenix23707@gmail.com>
2026-02-01 12:10:41 +0300

    Init commit

---
+++ src/main/java/by/fnx/market/analyzer/MarketAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package by.fnx.market.analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/fnx/market/analyzer/MarketAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package by.fnx.market.analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
